4a148986127a58238afea9ec12b418b6a11f5125
AndreasBP <andreasbpedersen1@gmail.com>
2026-02-25 19:44:16 +0100

    Recipe App Boiler

---
+++ app/(tabs)/index.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+
+
+export default function DiscoverScreen() {
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      
+     
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    backgroundColor: '#F9F8F4' 
+  },
+});
